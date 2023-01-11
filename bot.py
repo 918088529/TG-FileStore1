f5d48acc954ed59d0775a53e2571701072d9965c
SHOKDHAR <121552501+918088529@users.noreply.github.com>
2023-01-11 20:01:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def start(bot: Client, cmd: Message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("BotsList Channel", url="https://t.me/TGRobot_List")
+                        InlineKeyboardButton("BotsList Channel", url="https://t.me/https:/ideal_bot_father")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
@@ -82,8 +82,8 @@ async def start(bot: Client, cmd: Message):
                         InlineKeyboardButton("Close 🚪", callback_data="closeMessage")
                     ],
                     [
-                        InlineKeyboardButton("Bots Channel", url="https://t.me/TeleRoidGroup"),
-                        InlineKeyboardButton(" Support Group", url="https://t.me/TeleRoid14")
+                        InlineKeyboardButton("Bots Channel", url="https:https://t.me/ideal_bot_father"),
+                        InlineKeyboardButton(" Support Group", url="https://t.me/Chart_122122_bot")
                     ]
                 ]
             )
